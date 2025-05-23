1d702cbbec13119a021ed356c28d4047ca03576f
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-05-23 00:52:09 -0300

    feat: cria inputs

---
+++ src/main/java/br/com/dio/ui/custom/input/NumberText.java
@@ -0,0 +1,60 @@
+package br.com.dio.ui.custom.input;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import br.com.dio.model.Space;
+
+public class NumberText extends JTextField {
+    private final Space space;
+
+    public NumberText(final Space space) {
+        this.space = space;
+        var dimension = new Dimension(50, 50);
+
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
+        this.setVisible(true);
+        this.setFont(new Font("Arial", Font.PLAIN, 20));
+        this.setHorizontalAlignment(JTextField.CENTER);
+        this.setDocument(new NumberTextLimit());
+        this.setEnabled(!space.isFixed());
+        if (space.isFixed()) {
+            this.setText(space.getActual().toString());
+        }
+        this.getDocument().addDocumentListener(new DocumentListener() {
+            
+            private void changeSpace() {
+                if (getText().isEmpty()) {
+                    space.clearSpace();
+                    return;
+                }
+                space.setActual(Integer.parseInt(getText()));
+
+            }
+
+            @Override
+            public void insertUpdate(final DocumentEvent e) {
+                changeSpace();
+
+                // space.setActual(0);
+            }
+
+            @Override
+            public void removeUpdate(final DocumentEvent e) {
+                changeSpace();
+
+                // space.setActual(0);
+            }
+
+            @Override
+            public void changedUpdate(final DocumentEvent e) {
+                changeSpace();
+            }
+        });
+    }
+}
